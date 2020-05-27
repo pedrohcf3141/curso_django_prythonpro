f8b461addcb1c0093746d14b1bd1670ce91659f1
Pedro Henrique Cerqueira Fernandes <pedrohcf3.14@gmail.com>
2020-05-26 22:02:26 -0300

    setado SECRET_KEY no settings e no env-sample

--- pythonpro/settings.py
+++ pythonpro/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l(hvf+3o9k$3h9m&uf_@5*f%qdy8a(@!(+j0r(+tzpjepkx@54'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
