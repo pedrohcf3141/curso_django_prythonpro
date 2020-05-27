bf768a95de22a63267aef868a2c7489419319fff
Pedro Henrique Cerqueira Fernandes <pedrohcf3.14@gmail.com>
2020-05-26 21:33:54 -0300

    criado arquivo env-sample

--- pythonpro/settings.py
+++ pythonpro/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l(hvf+3o9k$3h9m&uf_@5*f%qdy8a(@!(+j0r(+tzpjepkx@54'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
