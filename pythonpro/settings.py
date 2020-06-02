4513a4429718736d6849935d0a7f07a05e2ffc2f
Pedro Henrique Cerqueira Fernandes <pedrohcf3.14@gmail.com>
2020-06-02 10:47:10 -0300

    Configuração do idioma e fuso horario
    Configuração dos arquivos de desenvolvimento static  e media

--- pythonpro/settings.py
+++ pythonpro/settings.py
@@ -108,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -122,4 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+#configuração do ambiente de desenvolvimento
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
