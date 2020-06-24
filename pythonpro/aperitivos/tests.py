63af88f508b2b3f3c7535e0d41a6cf20fc69101a
Pedro Henrique Cerqueira Fernandes <pedrohcf3.14@gmail.com>
2020-06-23 21:25:57 -0300

    ajustado heranças dos templates ajustados os testes devido a herença

--- pythonpro/aperitivos/tests.py
+++ pythonpro/aperitivos/tests.py
@@ -1,6 +1,6 @@
 import pytest
 from django.urls import reverse
-
+from pythonpro.django_assertions import assert_contains
 
 @pytest.fixture
 def resp(client):
@@ -9,3 +9,9 @@ def resp(client):
 
 def test_status_code(resp):
     assert resp.status_code == 200
+
+def test_titulo_video(resp):
+    assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>')
+
+def test_conteudo_video(resp):
+    assert_contains(resp, '<iframe width="560" height="315" src="https://www.youtube.com/embed/r9xU2r8ryzI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
